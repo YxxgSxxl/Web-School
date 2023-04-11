2d60aa38a317be306b8eaf5368d59e9d97e5a7eb
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2023-04-11 11:51:55 +0200

    V1

--- S2/E-Portfolio SAE202/js/index.js
+++ S2/E-Portfolio SAE202/js/index.js
@@ -4,5 +4,9 @@ let enter = document.querySelector("div.enterwired");
 enter.addEventListener('click', () => {
     document.querySelector("div.enterwired").remove();
 
-    document.querySelector("source").src="./img/video/back-vid-2.mp4";
+    document.querySelector(".back-vid").innerHTML = `<div class="back-vid">
+    <video autoplay muted plays-inline class="back-vid">
+        <source src="./img/video/back-vid-2.mp4" type="video/mp4">
+    </video>
+</div>`;
 })
