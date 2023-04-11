88d8198f8bb1251ed8eee6c74dbaf9977ed8b529
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2023-04-11 10:49:34 +0200

    V1

--- S2/E-Portfolio SAE202/js/index.js
+++ S2/E-Portfolio SAE202/js/index.js
@@ -3,4 +3,6 @@ let enter = document.querySelector("div.enterwired");
 
 enter.addEventListener('click', () => {
     document.querySelector("div.enterwired").remove();
+
+    document.querySelector("source").src="./img/video/back-vid-2.mp4";
 })
