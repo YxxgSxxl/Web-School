ac25cea01253c9e2cd21c35a66e8903349084618
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2023-05-17 09:01:14 +0200

    V1

--- S2/E-Portfolio SAE202/js/sae.js
+++ S2/E-Portfolio SAE202/js/sae.js
@@ -1,9 +1,39 @@
+
 setTimeout(matrix, 2000);
 setTimeout(show2steps, 1500);
 setInterval(function(){ decodeText(); }, 1000);
 
+let slideL = document.querySelector("div.carousel-slideL svg");
+let slideR = document.querySelector("div.carousel-slideR svg");
+
 document.querySelector(".page_show").classList.add("show");
-document.querySelector("footer").classList.add("show");
+// document.querySelector("footer").classList.add("show");
+
+// Slide carousel RIGHT button
+let R = 0;
+const carouselElements = document.querySelector(".carousel-elements");
+
+slideR.addEventListener('click', () => {
+    console.log(carouselElements.offsetWidth);
+    console.log(R)
+    if (Math.abs(R) > (carouselElements.scrollWidth - carouselElements.offsetWidth)) {
+        return;
+    }
+    R -= 300;
+    carouselElements.style.transform = "translateX(" + R.toString() + "px)";
+})
+
+// Slide carousel LEFT button
+
+slideL.addEventListener('click', () => {
+    console.log(carouselElements.offsetWidth);
+    console.log(R)
+    if (Math.abs(R) <= 0) {
+        return;
+    }
+    R += 300;
+    carouselElements.style.transform = "translateX(" + R.toString() + "px)";
+})
 
 // Matrix Background Effect
 function matrix() {
@@ -170,5 +200,5 @@ function shuffle(array) {
 
 // Show the footer with little animations (FOR ABOUT PAGE)
 function show2steps() {
-    document.querySelector("footer").classList.add("show2");
+    // document.querySelector("footer").classList.add("show2");
 }
