7091a2aefc63cef9df9c77e59652315e2fe8e3ef
YungSoul <eckes.alex14@gmail.com>
2024-02-10 11:08:45 +0100

    fetches

--- S4/Front-End/TD1-Requêtes-asynchrones-Fetch/second-try/js/index.js
+++ S4/Front-End/TD1-Requêtes-asynchrones-Fetch/second-try/js/index.js
@@ -1,8 +1,19 @@
+let div = document.querySelector("div");
+
 fetch('fetch.php')        // Appel à un fichier.
 .then(function(response) {  // Prétraitement de la réponse.
      return response.text(); 
 })
 .then(function(txt) {       // Utilisation de la réponse.
-    document.querySelector("div").innerText = txt;
+    document.querySelector("div").innerText += txt;
+    // Du code JS pour traiter la réponse.
+});
+
+fetch('fetch.json')        // Appel à un fichier.
+.then(function(response) {  // Prétraitement de la réponse.
+     return response.text(); 
+})
+.then(function(txt) {       // Utilisation de la réponse.
+    document.querySelector("div").innerText += txt;
     // Du code JS pour traiter la réponse.
 });
