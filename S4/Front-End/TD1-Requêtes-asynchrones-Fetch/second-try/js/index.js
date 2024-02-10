17f9f374a5ab836a1483c278b43202704c9a07e6
YungSoul <eckes.alex14@gmail.com>
2024-02-10 11:12:21 +0100

    updated

--- S4/Front-End/TD1-Requêtes-asynchrones-Fetch/second-try/js/index.js
+++ S4/Front-End/TD1-Requêtes-asynchrones-Fetch/second-try/js/index.js
@@ -1,19 +1,20 @@
-let div = document.querySelector("div");
+// let div = document.querySelector("div");
 
-fetch('fetch.php')        // Appel à un fichier.
-.then(function(response) {  // Prétraitement de la réponse.
-     return response.text(); 
-})
-.then(function(txt) {       // Utilisation de la réponse.
-    document.querySelector("div").innerText += txt;
-    // Du code JS pour traiter la réponse.
-});
+// fetch('fetch.php')        // Appel à un fichier.
+// .then(function(response) {  // Prétraitement de la réponse.
+//      return response.text(); 
+// })
+// .then(function(txt) {       // Utilisation de la réponse.
+//     document.querySelector("div").innerText += txt;
+//     // Du code JS pour traiter la réponse.
+// });
 
-fetch('fetch.json')        // Appel à un fichier.
-.then(function(response) {  // Prétraitement de la réponse.
-     return response.text(); 
-})
-.then(function(txt) {       // Utilisation de la réponse.
-    document.querySelector("div").innerText += txt;
-    // Du code JS pour traiter la réponse.
-});
+// fetch('fetch.json')        // Appel à un fichier.
+// .then(function(response) {  // Prétraitement de la réponse.
+//      return response.text(); 
+// })
+// .then(function(txt) {       // Utilisation de la réponse.
+//     document.querySelector("div").innerText += txt;
+//     // Du code JS pour traiter la réponse.
+// });
+
