11821f12d20c0a75917a49d58be31a0951a7871e
YungSoul <eckes.alex14@gmail.com>
2024-02-15 14:40:28 +0100

    Canvas Added

--- S4/Design-Interactif/TD5/js/app.js
+++ S4/Design-Interactif/TD5/js/app.js
@@ -16,7 +16,10 @@ let x = 0;
 let y = 0;
 
 let xCube = 600;
-let yCube = 100;
+let yCube = 500;
+
+let vxCube = 1.2;
+let vyCube = 1.2;
 
 let deltaX = 0;
 let deltaY = 0;
@@ -24,15 +27,16 @@ let deltaY = 0;
 afficher();
 
 function update() {
-    xCube += 2;
-    yCube += 2;
+    x += 2;
+    y += 2;
 }
 
 function afficher() {
     // Cube Canvas init
-    ctx.fillstyle = "blue";
+    ctx.clearRect(0, 0, w, h); // Efface le canvas avant de redessiner
+    ctx.fillStyle = "blue";
     ctx.fillRect(xCube, yCube, 20, 20);
-    ctx.moveTo(100, 200);
+    ctx.moveTo(x, y);
 
     // requestAnimationFrame
     window.requestAnimationFrame(update);
@@ -43,29 +47,32 @@ body.addEventListener('keydown', keyPress);
 
 // Fonction pour savoir quel touche est pressée
 function keyPress(e) {
+    e.preventDefault(); // Ajouté pour empêcher le comportement par défaut des touches (scroll, etc.)
     e.stopPropagation();
     // console.log(e.key); // Debug
     switch (e.key) {
         case 'ArrowUp':
-            y -= +100;
+            yCube -= 10 * vxCube; // Modification pour déplacer le cube sur le canvas
             console.log("up");
             break;
         case 'ArrowRight':
-            x += 100;
+            xCube += 10;
             console.log("right");
             break;
         case 'ArrowDown':
-            y -= -100;
+            yCube += 10;
             console.log("down");
             break;
-            case 'ArrowLeft':
-            x -= 100;
+        case 'ArrowLeft':
+            xCube -= 10;
             console.log("left");
             break;
         case 'm':
             body.remove();
         default:
+            break;
     }
+    afficher(); // Redessine le cube après le déplacement
     cube.style.transform = `translate(${x}px, ${y}px)`;
 }
 
